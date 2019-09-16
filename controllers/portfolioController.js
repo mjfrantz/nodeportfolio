b836e5978b1a5de4b1f4fb56b5a6ad1fa5f6efd0
Michael Frantz <mjfrantz26@gmail.com>
2019-09-16 12:07:57 +0700

    Finished CRUD operations

--- controllers/portfolioController.js
+++ controllers/portfolioController.js
@@ -1,30 +1,40 @@
 const Portfolio = require('./../models/portfolioModel');
 
-exports.getAllPortfolios = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined'
-    });
+exports.getAllPortfolios = async (req, res) => {
+    try {
+        const portfolios = await Portfolio.find();
+
+        res.status(200).json({
+            status: 'success',
+            results: portfolios.length,
+            data: {
+                portfolios
+            }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'Fail',
+            message: err
+        });
+    }
 };
 
-exports.getPortfolio = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined'
-    });
-};
-exports.updatePortfolio = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined'
-    });
-};
+exports.getPortfolio = async (req, res) => {
+    try {
+        const portfolio = await Portfolio.findById(req.params.id);
 
-exports.deletePortfolio = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: ' This route is not yet defined'
-    });
+        res.status(200).json({
+            status: 'success',
+            data: {
+                portfolio
+            }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'SuperFail',
+            message: err
+        });
+    }
 };
 
 exports.createPortfolio = async (req, res) => {
@@ -36,6 +46,45 @@ exports.createPortfolio = async (req, res) => {
             data: {
                 portfolio: newPortfolio
             }
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err
+        });
+    }
+};
+
+exports.updatePortfolio = async (req, res) => {
+
+    try {
+
+        const portfolio = await Portfolio.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidator: true
+        })
+        res.status(200).json({
+            status: 'success',
+            data: {
+                portfolio
+            }
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err
+        });
+    }
+};
+
+exports.deletePortfolio = async (req, res) => {
+
+    await Portfolio.findByIdAndDelete(req.params.id)
+
+    try {
+        res.status(204).json({
+            status: 'success',
+            data: null
         });
     } catch (err) {
         res.status(400).json({
